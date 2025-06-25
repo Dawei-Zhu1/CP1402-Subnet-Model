2ebce58475829f9e4cae6ca0f0ba507ec71531b1
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-06-26 00:39:24 +1000

    Refactor the key control logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // script.js
 import {IP} from './IP.js';
-import * as CONSTANTS from './constants.js';
+// import * as CONSTANTS from './constants.js';
 import {Layout} from './layout.js'
 
 const ip = new IP()
@@ -9,34 +9,41 @@ layout.refreshAll()
 
 document.addEventListener('keydown',
     (event) => {
-        let keyname = event.key,
-            keycode = event.code
+        let keyName = event.key
+        // let keycode = event.code
         event.preventDefault()
-        // Ctrl + ArrLeft
-        if ((event.ctrlKey || event.metaKey) && event.key === 'ArrowLeft') {
-            layout.moveSubnetBoundaryLeftmost()
-        } else
-            // Ctrl + ArrRight
-        if ((event.ctrlKey || event.metaKey) && event.key === 'ArrowRight') {
-            layout.moveSubnetBoundaryRightmost()
-        } else
-            // Ctrl + A
-        if ((event.ctrlKey || event.metaKey) && event.key === 'a') {
-            layout.moveClassBoundaryLeftmost()
-        } else
-            // Ctrl + D
-        if ((event.ctrlKey || event.metaKey) && event.key === 'd') {
-            layout.moveClassBoundaryRightmost()
+        // Ctrl/CMD + Key
+        if (event.ctrlKey || event.metaKey) {
+            switch (keyName) {
+                case 'ArrowLeft':
+                    layout.moveSubnetBoundaryLeftmost()
+                    break;
+                case 'ArrowRight':
+                    layout.moveSubnetBoundaryRightmost()
+                    break;
+                case 'a':
+                    layout.moveClassBoundaryLeftmost()
+                    break;
+                case 'd':
+                    layout.moveClassBoundaryRightmost()
+                    break;
+            }
         } else
             // Single key
-        if (keyname === 'ArrowLeft') {
-            layout.moveSubnetBoundary(-1)
-        } else if (keyname === 'ArrowRight') {
-            layout.moveSubnetBoundary()
-        } else if (keyname === 'a') {
-            layout.moveClassBoundary(-1)
-        } else if (keyname === 'd') {
-            layout.moveClassBoundary()
-        }
+            switch (keyName) {
+                case'ArrowLeft':
+                    layout.moveSubnetBoundary(-1)
+                    break;
+                case 'ArrowRight':
+                    layout.moveSubnetBoundary()
+                    break;
+                case 'a':
+                    layout.moveClassBoundary(-1)
+                    break;
+                case 'd':
+                    layout.moveClassBoundary()
+                    break;
+            }
+
     }
 )
